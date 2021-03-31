b93155759a9a1f657a5352119fa641dc95a86353
FilipSpirovski <spirovskii.filipp@gmail.com>
2021-03-31 20:00:01 +0200

    project restructuring

--- model/repair_station.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-
-class RepairStation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    available = db.Column(db.Boolean, nullable=False)
-    address_id = db.Column(db.Integer, db.ForeignKey('address.id'), nullable=False)  # OneToOne
