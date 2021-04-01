1847667d309ddbc8c52430d23687d8777058ebaf
FilipSpirovski <spirovskii.filipp@gmail.com>
2021-04-01 16:18:21 +0200

    parking zone endpoints added

--- models.py
+++ models.py
@@ -27,30 +27,16 @@ class ParkingZone(db.Model):
 
 class Address(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    street_id = db.Column(db.Integer, db.ForeignKey('street.id'), nullable=False)  # OneToOne
-    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)  # OneToOne
+    street_name = db.Column(db.String(50), nullable=False)
+    street_number = db.Column(db.Integer, nullable=False)
+    city_name = db.Column(db.String(25), nullable=False)
+    city_postal_code = db.Column(db.Integer, nullable=False)
     country = db.Column(db.String(60), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)  # OneToOne
+
     parking_zone = db.relationship('ParkingZone', backref='address', uselist=False)
     bicycle_store = db.relationship('BicycleStore', backref='address', uselist=False)
     repair_station = db.relationship('RepairStation', backref='address', uselist=False)
-
-
-address_to_city = db.Table('address_to_city',
-                           db.Column('address_id', db.Integer, db.ForeignKey('address.id')),
-                           db.Column('city_postal_code', db.Integer, db.ForeignKey('city.postal_code')))
-
-
-class City(db.Model):
-    postal_code = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(25), unique=True, nullable=False)
-    addresses = db.relationship('Address', secondary=address_to_city, backref=db.backref('cities'))
-
-
-class Street(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    number = db.Column(db.Integer, nullable=False)
-    address = db.relationship('Address', backref='street', uselist=False)
 
 
 class Location(db.Model):
